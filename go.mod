63ba37b74114b759277bace8581a306528a4ed5c
Asyraf Duyshart <me@asyraf.id>
2020-09-11 16:14:24 +0700

    - fix iphone using

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/asyrafduyshart/go-mobile-redirect
+
+go 1.14
+
+require github.com/mssola/user_agent v0.5.2 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
+
+	"github.com/mssola/user_agent"
 )
 
 // RedirectType data to redirect
@@ -20,17 +21,7 @@ type RedirectType struct {
 var URLRedirect map[string]RedirectType
 
 func isMobile(useragent string) bool {
-	// the list below is taken from
-	// https://github.com/bcit-ci/CodeIgniter/blob/develop/system/libraries/User_agent.php
-
-	mobiles := []string{"Mobile Explorer", "Palm", "Motorola", "Nokia", "Palm", "Apple iPhone", "iPad", "Apple iPod Touch", "Sony Ericsson", "Sony Ericsson", "BlackBerry", "O2 Cocoon", "Treo", "LG", "Amoi", "XDA", "MDA", "Vario", "HTC", "Samsung", "Sharp", "Siemens", "Alcatel", "BenQ", "HP iPaq", "Motorola", "PlayStation Portable", "PlayStation 3", "PlayStation Vita", "Danger Hiptop", "NEC", "Panasonic", "Philips", "Sagem", "Sanyo", "SPV", "ZTE", "Sendo", "Nintendo DSi", "Nintendo DS", "Nintendo 3DS", "Nintendo Wii", "Open Web", "OpenWeb", "Android", "Symbian", "SymbianOS", "Palm", "Symbian S60", "Windows CE", "Obigo", "Netfront Browser", "Openwave Browser", "Mobile Explorer", "Opera Mini", "Opera Mobile", "Firefox Mobile", "Digital Paths", "AvantGo", "Xiino", "Novarra Transcoder", "Vodafone", "NTT DoCoMo", "O2", "mobile", "wireless", "j2me", "midp", "cldc", "up.link", "up.browser", "smartphone", "cellphone", "Generic Mobile"}
-
-	for _, device := range mobiles {
-		if strings.Index(useragent, device) > -1 {
-			return true
-		}
-	}
-	return false
+	return user_agent.New(useragent).Mobile()
 }
 
 // Simple helper function to read an environment or return a default value
